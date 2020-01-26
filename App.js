e2def8629771ac7d2d3719f415d4cc26b634db1c
Sumeer Tuli <sumeertuli7@gmail.com>
2020-01-25 20:31:13 -0800

    clear code for todo app

--- App.js
+++ App.js
@@ -8,34 +8,9 @@ import {
 } from 'react-native';
 
 export default function App() {
-  const [people, setPeople] = useState([
-    { name: 'shaun', id: '1' },
-    { name: 'yoshi', id: '2' },
-    { name: 'mario', id: '3' },
-    { name: 'luigi', id: '4' },
-    { name: 'peach', id: '5' },
-    { name: 'toad', id: '6' },
-    { name: 'bowser', id: '7' },
-  ]);
-
-  const pressHandler = id => {
-    setPeople(prevState => {
-      return prevState.filter(({id: originalId}) => originalId !== id)
-    })
-  }
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data ={people}
-        keyExtractor={item => item.id}
-        numColumns={2}
-        renderItem={({ item: {id, name} }) => (
-          <TouchableOpacity onPress={() => pressHandler(id)}>
-            <Text style={styles.item}>{name}</Text>
-          </TouchableOpacity>
-        )}
-        />
     </View>
   );
 }
@@ -43,16 +18,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 40,
-    paddingHorizontal: 20,
     backgroundColor: '#fff'
   },
-  item: {
-    flex: 1,
-    marginHorizontal: 10,
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: 'pink',
-    fontSize: 18,
-  }
 });
