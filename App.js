3d87f6e592cbd0fc811c900e72bb3ec7a5fce76e
Sumeer Tuli <sumeertuli7@gmail.com>
2020-01-25 16:36:23 -0800

    initial

--- App.js
+++ App.js
@@ -4,7 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      
+      <View style={styles.header}>
+        <Text style={styles.text}>Hello World</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.text}>Lorem Ipsum yada yada</Text>
+        <Text>Lorem Ipsum yada yada</Text>
+        <Text>Lorem Ipsum yada yada</Text>
+      </View>
     </View>
   );
 }
@@ -16,4 +23,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    backgroundColor: 'orange',
+    padding: 20
+  },
+  body: {
+    backgroundColor: 'green',
+    padding: 20
+  },
+  text: {
+    fontWeight: 'bold'
+  }
 });
