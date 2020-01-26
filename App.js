00884ec93fc9a5251e03b13448ddf2e1550e7ce7
Sumeer Tuli <sumeertuli7@gmail.com>
2020-01-25 20:54:31 -0800

    Adding header

--- App.js
+++ App.js
@@ -6,11 +6,29 @@ import {
   TouchableOpacity,
   View
 } from 'react-native';
+import Header from './components/Header'
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'buy coffee', key: '1' },
+    { text: 'create an app', key: '2' },
+    { text: 'play on the switch', key: '3' }
+  ]);
 
   return (
     <View style={styles.container}>
+      <Header />
+      <View style={styles.content}>
+        { /* to form */ }
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item: { text } }) => (
+              <Text>{text}</Text>
+            )}
+            />
+        </View>
+      </View>
     </View>
   );
 }
@@ -20,4 +38,10 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff'
   },
+  content: {
+    padding: 40
+  },
+  list: {
+    marginTop: 20
+  }
 });
